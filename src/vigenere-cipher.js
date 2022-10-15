4f492acb18824f60499af56b299599b2e425e630
MarinaStepanchuk <mari20002@mail.ru>
2022-10-16 01:04:57 +0300

    feat: common-character-count

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,11 +20,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
+  encrypt(message, key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
+  decrypt(encryptedMessage, key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
